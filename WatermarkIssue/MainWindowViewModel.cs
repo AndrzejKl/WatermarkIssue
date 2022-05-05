4ada35052182fa907841c374ec6b7ad9bc95c87b
AndrzejKl <andrzej.klonowski@gmail.com>
2022-05-05 22:03:09 +0200

    Dodaj pliki projektów.

---
+++ WatermarkIssue/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace WatermarkIssue
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private string? myProperty = "Display watermark";
+        private string? myProperty1 = "fixed watermark";
+
+        [Display(Name = "Watermark")]
+        public string? MyProperty { get => myProperty; set => SetProperty(ref myProperty, value); }
+
+        public string? MyProperty1 { get => myProperty1; set => SetProperty(ref myProperty1, value); }
+    }
+}
